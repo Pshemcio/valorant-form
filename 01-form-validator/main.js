96c8bd331eb4c0dfba883c7efe92bcf5f838cfcd
Pshemcio <daglas92@gmail.com>
2020-12-11 11:58:16 +0100

    Change layout concept to Valorant website

--- 01-form-validator/main.js
+++ 01-form-validator/main.js
@@ -18,19 +18,10 @@ const showSuccess = input => {
     formControl.className = 'form-control success';
 }
 
-const checkEmail = input => {
-    const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-
-    if (re.test(String(input.value).trim())) {
-        showSuccess(input);
-    } else {
-        showError(input, 'To nie jest adres email.')
-    };
-};
-
 //check required
 const checkRequired = (inputArr) => {
     inputArr.forEach(input => {
+        //Checks if input has value
         if (input.value.trim() === '') {
             if (input.id === 'password2') {
                 showError(input, `${getFieldName(input)}`)
@@ -39,10 +30,36 @@ const checkRequired = (inputArr) => {
 
             showError(input, `Musisz wpisać ${getFieldName(input)}`)
 
-        } else {
+        } else if (input.value.length > 0) {
             showSuccess(input);
+
+            // final input validation
+            switch (input.id) {
+                case username.id:
+                    checkLength(input, 3, 15);
+                    break;
+                case password.id:
+                    checkLength(input, 6, 25);
+                    break;
+                case email.id:
+                    checkEmail(input);
+                    break;
+                case password2.id:
+                    checkPasswordMatch(password, input);
+                    break;
+            };
         };
     });
+};
+
+const checkEmail = input => {
+    const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    if (re.test(String(input.value).trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'To nie jest adres email.')
+    };
 };
 
 //check passwords match
@@ -69,15 +86,9 @@ const checkLength = (input, min, max) => {
     };
 };
 
-
 //Event listeners
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     checkRequired([username, email, password, password2]);
-
-    checkLength(username, 3, 15);
-    checkLength(password, 6, 25);
-    checkEmail(email);
-    checkPasswordMatch(password, password2);
 });
