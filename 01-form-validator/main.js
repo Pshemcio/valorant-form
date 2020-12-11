5679751a9783fd297bb9b97fdc90856515ed98d9
Pshemcio <daglas92@gmail.com>
2020-12-11 16:22:04 +0100

    Make email works and validate

--- 01-form-validator/main.js
+++ 01-form-validator/main.js
@@ -1,94 +1,140 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //Event listeners
+
+    // form.addEventListener('click', (e) => {
+    //     e.preventDefault();
+    //     checkRequired([email, username, password, password2]);
+    // });
+
+    inputs.forEach(input => {
+        input.addEventListener('keyup', checkRequired)
+        input.addEventListener('focus', movePlaceholderUp);
+        input.addEventListener('blur', movePlaceholderBack);
+    })
+
+    nextPageBtns.forEach(button => {
+        button.addEventListener('click', checkRequired);
+    });
+
+
+    autoFocus(email);
+});
+
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
+const inputs = document.querySelectorAll('.inner-wrap input');
+const nextPageBtns = document.querySelectorAll('.next-page');
 
-//Show input error msg
-const showError = (input, msg) => {
-    const formControl = input.parentElement;
-    formControl.className = 'form-control error';
-    const small = formControl.querySelector('small');
-    small.innerText = msg;
-};
-
-//show success outline
-const showSuccess = input => {
-    const formControl = input.parentElement;
-    formControl.className = 'form-control success';
+//Select input on page load
+const autoFocus = input => {
+    input.focus();
 }
 
-//check required
-const checkRequired = (inputArr) => {
-    inputArr.forEach(input => {
-        //Checks if input has value
-        if (input.value.trim() === '') {
-            if (input.id === 'password2') {
-                showError(input, `${getFieldName(input)}`)
-                return
-            };
+//move placeholder text up while on focus
+const movePlaceholderUp = input => {
+    input.target.classList.add('input-active')
+}
 
-            showError(input, `Musisz wpisać ${getFieldName(input)}`)
+const movePlaceholderBack = input => {
+    if (input.target.value !== '') {
+        return;
+    }
+    input.target.classList.remove('input-active');
+}
 
-        } else if (input.value.length > 0) {
-            showSuccess(input);
-
-            // final input validation
-            switch (input.id) {
-                case username.id:
-                    checkLength(input, 3, 15);
-                    break;
-                case password.id:
-                    checkLength(input, 6, 25);
-                    break;
-                case email.id:
-                    checkEmail(input);
-                    break;
-                case password2.id:
-                    checkPasswordMatch(password, input);
-                    break;
-            };
-        };
-    });
-};
+//change input color and next-button event in case succes/error
 
 const checkEmail = input => {
     const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    const button = input.closest('form').querySelector('.next-page');
 
     if (re.test(String(input.value).trim())) {
-        showSuccess(input);
-    } else {
-        showError(input, 'To nie jest adres email.')
+        input.classList.remove('error');
+        button.classList.add('btn-validate')
+    }
+    else {
+        input.classList.add('error')
+        button.classList.remove('btn-validate')
     };
 };
 
-//check passwords match
-const checkPasswordMatch = (input1, input2) => {
-    if (input1.value !== input2.value) {
-        showError(input2, 'Hasła się nie zgadzają!')
-    };
+const checkRequired = input => {
+    input.preventDefault();
+    const actualinput = input.target.closest('form').querySelector('input[type="text"]');
+    checkEmail(actualinput)
 };
 
-const getFieldName = (input) => {
-    if (input.id === 'password2') {
-        return 'Musisz powtórzyć hasło'
+
+
+
+
+const tempDisable = () => {
+    //show success outline
+    const showSuccess = input => {
+        const formControl = input.parentElement;
+        formControl.className = 'form-control success';
+    }
+
+    //check required
+    const checkRequired = (inputArr) => {
+        inputArr.forEach(input => {
+            //Checks if input has value
+            if (input.value.trim() === '') {
+                if (input.id === 'password2') {
+                    showError(input, `${getFieldName(input)}`)
+                    return
+                };
+
+                showError(input, `Musisz wpisać ${getFieldName(input)}`)
+
+            } else if (input.value.length > 0) {
+                showSuccess(input);
+
+                // final input validation
+                switch (input.id) {
+                    case username.id:
+                        checkLength(input, 3, 15);
+                        break;
+                    case password.id:
+                        checkLength(input, 6, 25);
+                        break;
+                    case email.id:
+                        checkEmail(input);
+                        break;
+                    case password2.id:
+                        checkPasswordMatch(password, input);
+                        break;
+                };
+            };
+        });
     };
 
-    return input.placeholder.charAt(6).toUpperCase() + input.placeholder.slice(7);
-};
 
-//check input length
-const checkLength = (input, min, max) => {
-    if (input.value.length < min) {
-        showError(input, `${getFieldName(input)} musi mieć conajmniej ${min} znaków!`)
-    } else if (input.value.length >= max) {
-        showError(input, `${getFieldName(input)} nie może mieć więcej niż ${min} znaków!`)
+
+    //check passwords match
+    const checkPasswordMatch = (input1, input2) => {
+        if (input1.value !== input2.value) {
+            showError(input2, 'Hasła się nie zgadzają!')
+        };
     };
-};
 
-//Event listeners
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+    const getFieldName = (input) => {
+        if (input.id === 'password2') {
+            return 'Musisz powtórzyć hasło'
+        };
 
-    checkRequired([username, email, password, password2]);
-});
+        return input.placeholder.charAt(6).toUpperCase() + input.placeholder.slice(7);
+    };
+
+    //check input length
+    const checkLength = (input, min, max) => {
+        if (input.value.length < min) {
+            showError(input, `${getFieldName(input)} musi mieć conajmniej ${min} znaków!`)
+        } else if (input.value.length >= max) {
+            showError(input, `${getFieldName(input)} nie może mieć więcej niż ${min} znaków!`)
+        };
+    };
+}
