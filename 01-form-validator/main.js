72f47d60bc222851a2792b69f47a2ec67d726c1b
Pshemcio <daglas92@gmail.com>
2020-12-11 09:59:31 +0100

    Make form works and validate

---
+++ 01-form-validator/main.js
@@ -0,0 +1,83 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+//Show input error msg
+const showError = (input, msg) => {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = msg;
+};
+
+//show success outline
+const showSuccess = input => {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+const checkEmail = input => {
+    const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    if (re.test(String(input.value).trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'To nie jest adres email.')
+    };
+};
+
+//check required
+const checkRequired = (inputArr) => {
+    inputArr.forEach(input => {
+        if (input.value.trim() === '') {
+            if (input.id === 'password2') {
+                showError(input, `${getFieldName(input)}`)
+                return
+            };
+
+            showError(input, `Musisz wpisać ${getFieldName(input)}`)
+
+        } else {
+            showSuccess(input);
+        };
+    });
+};
+
+//check passwords match
+const checkPasswordMatch = (input1, input2) => {
+    if (input1.value !== input2.value) {
+        showError(input2, 'Hasła się nie zgadzają!')
+    };
+};
+
+const getFieldName = (input) => {
+    if (input.id === 'password2') {
+        return 'Musisz powtórzyć hasło'
+    };
+
+    return input.placeholder.charAt(6).toUpperCase() + input.placeholder.slice(7);
+};
+
+//check input length
+const checkLength = (input, min, max) => {
+    if (input.value.length < min) {
+        showError(input, `${getFieldName(input)} musi mieć conajmniej ${min} znaków!`)
+    } else if (input.value.length >= max) {
+        showError(input, `${getFieldName(input)} nie może mieć więcej niż ${min} znaków!`)
+    };
+};
+
+
+//Event listeners
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    checkRequired([username, email, password, password2]);
+
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordMatch(password, password2);
+});
