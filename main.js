716f1062dd31899af32481aa3fd7845abf9c1163
Pshemcio <daglas92@gmail.com>
2020-12-12 14:07:56 +0100

    Add age verification

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     //Event listeners
-
-    // form.addEventListener('click', (e) => {
-    //     e.preventDefault();
-    //     checkRequired([email, username, password, password2]);
-    // });
-
     inputs.forEach(input => {
         input.addEventListener('keyup', checkRequired)
         input.addEventListener('focus', movePlaceholderUp);
@@ -19,6 +13,11 @@ document.addEventListener('DOMContentLoaded', () => {
     autoFocus(email);
 });
 
+let $counter = {
+    inputClassNumber: 2,
+    swipe: 0
+};
+
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
@@ -26,8 +25,9 @@ const birthday = document.getElementById('birthday');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 const inputs = document.querySelectorAll('.inner-wrap input');
-const nextPageBtns = document.querySelectorAll('.next-page');
+const nextPageBtns = form.querySelectorAll('.next-page');
 const pageEmail = document.querySelector('.page-email');
+const dateError = document.getElementById('date-error');
 
 //Select input on page load
 const autoFocus = input => {
@@ -49,7 +49,7 @@ const movePlaceholderBack = input => {
 //change input color and next-button event in case succes/error
 
 const basicValidator = (input, action) => {
-    const button = input.closest('form').querySelector('.next-page');
+    const button = input.closest('.form-control').querySelector('.next-page');
 
     if (action) {
         input.classList.remove('error');
@@ -57,6 +57,29 @@ const basicValidator = (input, action) => {
     } else {
         input.classList.add('error')
         button.classList.remove('btn-validate')
+    };
+};
+
+const getFieldName = (input) => {
+    if (input.id === 'password2') {
+        return 'Musisz powtórzyć hasło'
+    };
+
+    return input.placeholder.charAt(6).toUpperCase() + input.placeholder.slice(7);
+};
+
+const showError = (input, msg) => {
+    console.log('--------------------------')
+    console.log(input)
+    console.log(msg)
+};
+
+const checkLength = (input, min, max) => {
+    if (input.value.length < min) {
+        showError(input, `${getFieldName(input)} musi mieć conajmniej ${min} znaków!`)
+    } else if (input.value.length >= max) {
+        console.log(input)
+        // showError(input, `${getFieldName(input)} nie może mieć więcej niż ${min} znaków!`)
     };
 };
 
@@ -71,52 +94,97 @@ const checkEmail = input => {
     };
 };
 
-const checkDate = e => {
-    console.log(e.value)
-}
+const calcAge = date => {
+    const birthday = new Date(date.value);
+    const today = new Date();
+
+    const age = ((today - birthday) / 31557600000).toFixed(2);
+    birthday.max = new Date().toISOString().split("T")[0];
+
+    if (age === 'NaN') {
+        return
+    } else if (age > 130) {
+        basicValidator(date, false)
+        dateError.textContent = `You must enter valid date.`
+    } else if (age < 16) {
+        basicValidator(date, false)
+        dateError.textContent = `You must be at least 16 years old, be patient. :)`
+    } else {
+        basicValidator(date, true)
+        dateError.textContent = ``
+    }
+};
+
+const switchPage = (e) => {
+    const formControl = e.target.closest('.form-control');
+    console.log(e.target)
+    console.log(formControl)
+    console.log(formControl.parentElement)
+    form.style.transform = `translateY(-${$counter.swipe += 100}%)`
+    setTimeout(() => {
+        autoFocus(form.querySelector(`.input${$counter.inputClassNumber++}`));
+    }, 500);
+};
 
 const checkRequired = input => {
     input.preventDefault();
+
+    const btn = input.target.closest('.form-control').querySelector('.next-page');
+
+    if (btn.classList.contains('btn-validate') && input.key === 'Enter') {
+        switchPage(input);
+    };
+
     // const actualInput = input.target.closest('form').querySelector('input');
     const actualInput = input.target;
-
-    // console.log(actualInput);
-    console.log(actualInput.value)
 
     switch (actualInput.id) {
         case email.id:
             checkEmail(actualInput);
             break;
         case birthday.id:
-            checkDate(actualInput);
+            calcAge(actualInput);
             break;
         case username.id:
-            checkLength(input, 3, 15);
+            checkLength(actualInput, 3, 15);
             break;
-        // case password2.id:
-        //     checkPasswordMatch(password, input);
-        //     break;
     };
-
-    // checkEmail(actualInput);
 };
 
-const checkLength = (e) => {
-    console.log(e.target)
-}
 
 
 
-let test = 0;
 
-const switchPage = button => {
-    button.preventDefault();
-    form.style.transform = `translateY(-${test += 100}%)`
-    console.log(button.target)
-    setTimeout(() => {
-        autoFocus(birthday);
-    }, 400);
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // TEMP DISABLED !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -161,29 +229,10 @@ const tempDisable = () => {
         });
     };
 
-
-
     //check passwords match
     const checkPasswordMatch = (input1, input2) => {
         if (input1.value !== input2.value) {
             showError(input2, 'Hasła się nie zgadzają!')
         };
     };
-
-    const getFieldName = (input) => {
-        if (input.id === 'password2') {
-            return 'Musisz powtórzyć hasło'
-        };
-
-        return input.placeholder.charAt(6).toUpperCase() + input.placeholder.slice(7);
-    };
-
-    //check input length
-    const checkLength = (input, min, max) => {
-        if (input.value.length < min) {
-            showError(input, `${getFieldName(input)} musi mieć conajmniej ${min} znaków!`)
-        } else if (input.value.length >= max) {
-            showError(input, `${getFieldName(input)} nie może mieć więcej niż ${min} znaków!`)
-        };
-    };
-}
+};
